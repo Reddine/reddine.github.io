0b36eb5d91ece6131cee7055142de7220b4d431e
Kheir Eddine Farfar <kfarfar@gmail.com>
2025-03-04 21:18:03 +0100

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import prism from 'rehype-prism-plus';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: { resolve: { alias: { '@': '/src' } } },
+  vite: {
+    resolve: { alias: { '@': '/src' } },
+    plugins: [tailwindcss()],
+  },
   markdown: { syntaxHighlight: false, rehypePlugins: [prism] },
-  integrations: [tailwind()],
+  integrations: [],
   site: 'https://reddine.github.io',
 });
